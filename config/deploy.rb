15988cab1ac6f4dc204a3dcb075ae426a16d9080
Miguel Palhas <mpalhas@gmail.com>
2015-05-25 14:45:20 +0100

    Linking log directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ set :application, 'subvisual.co'
 set :stage, :production
 set :branch, (ENV['DEPLOY_BRANCH'] || :master)
 
+set :linked_dirs, %w(log)
+
 set :format, :pretty
 set :log_level, :debug
 set :pty, true
