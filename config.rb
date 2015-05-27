139531e03942fad87702e6eb6de5b4a07b4fdbde
Miguel Palhas <mpalhas@gmail.com>
2015-05-27 11:26:51 +0100

    Google verification fixed

--- config.rb
+++ config.rb
@@ -9,7 +9,6 @@ Slim::Engine.disable_option_validator!
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
@@ -23,6 +22,7 @@ Slim::Engine.disable_option_validator!
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+page '/google7c9e77390335f0f4.html', :layout => false, directory_index: false
 page 'sitemap.html', layout: false
 page 'sitemap.xml', layout: false
 page 'feed.xml', layout: false
