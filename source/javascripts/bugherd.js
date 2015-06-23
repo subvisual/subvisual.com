fdf49a71ce7e7931c3b1fd39538fe2deba25c998
Miguel Palhas <mpalhas@gmail.com>
2015-06-23 17:55:51 +0100

    Fixes JS file

--- source/javascripts/bugherd.js
+++ source/javascripts/bugherd.js
@@ -1,8 +1,6 @@
-<script type='text/javascript'>
 (function (d, t) {
  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
  bh.type = 'text/javascript';
  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=wvjvaxxdl56qeuzqhmda3g';
  s.parentNode.insertBefore(bh, s);
  })(document, 'script');
-</script>
