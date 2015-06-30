83293e42d9304bb21b0a9061b1af532a47276976
Miguel Palhas <mpalhas@gmail.com>
2015-06-30 16:50:18 +0100

    Disable imageoptim in deploys

--- config.rb
+++ config.rb
@@ -75,9 +75,9 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-  activate :imageoptim do |options|
-    options.pngout = false
-  end
+  # activate :imageoptim do |options|
+  #   options.pngout = false
+  # end
 
   # Use relative URLs
   # activate :relative_assets
