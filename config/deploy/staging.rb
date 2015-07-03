a8a63e95f2f36b2fb6fa1dbe3580a0ff6ffffdf2
Miguel Palhas <mpalhas@gmail.com>
2015-07-04 00:11:57 +0100

    Adds staging configs

---
+++ config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server 'test.subvisual.co', user: 'deploy', roles: %w{web}, primary: true
+set :deploy_to, '/apps/test.subvisual.co'
--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -1,3 +1,7 @@
 gb::nginx_conf { 'subvisual.co':
-  path => 'nginx.conf',
+  path => 'nginx.production.conf',
+}
+
+gb::nginx_conf { 'test.subvisual.co':
+  path => 'nginx.staging.conf',
 }
