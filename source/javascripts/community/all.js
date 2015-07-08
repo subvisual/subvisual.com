fdc2ad10ff7de768e73923e5287edac001db592a
João Justo <jpjustonunes@hotmail.com>
2015-07-08 19:02:52 +0100

    Fix animation depedencies

--- source/javascripts/community/all.js
+++ source/javascripts/community/all.js
@@ -1,3 +1,5 @@
-//= require gsap
 //= require jquery
+//= require gsap/src/minified/TweenLite.min
+//= require gsap/src/minified/easing/EasePack.min
+//= require gsap/src/minified/plugins/CSSPlugin.min
 //= require ./hero_animation
