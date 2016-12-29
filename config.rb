3a314563a6ae87ed340a98a09e0b097d2970135f
Miguel Palhas <mpalhas@gmail.com>
2016-12-29 13:51:29 +0000

    Updates config, and adds gzipped assets

--- config.rb
+++ config.rb
@@ -1,43 +1,10 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
 require 'slim'
 Slim::Engine.disable_option_validator!
-
-# Per-page layout changes:
-#
-# With no layout
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
 
 page '/google7c9e77390335f0f4.html', :layout => false, directory_index: false
 page 'sitemap.html', layout: false
 page 'sitemap.xml', layout: false
 page 'feed.xml', layout: false
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-###
-# Helpers
-# Built in custom helpers live in /helpers/custom_template_helpers.rb
-###
-
-# Reload the browser automatically whenever files change
-configure :development do
-  set :blog_posts_json, 'http://localhost:3000'
-end
 
 # Add bower's directory to sprockets asset path
 after_configuration do
@@ -46,7 +13,7 @@ after_configuration do
 end
 
 after_configuration do
- %w(javascripts stylesheets images).each do |dir|
+  %w(javascripts stylesheets images).each do |dir|
     sprockets.append_path File.join "#{root}", "vendor/blue/#{dir}"
   end
 
@@ -68,26 +35,25 @@ end
 activate :directory_indexes
 activate :es6
 
-# Build-specific configuration
+configure :development do
+  set :blog_posts_json, 'http://localhost:3000'
+end
+
 configure :build do
   set :blog_posts_json, 'https://subvisual.co'
 
-  # For example, change the Compass output style for deployment
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
-  # activate :imageoptim do |options|
-  #   options.pngout = false
-  # end
+  activate :gzip
 
-  # Use relative URLs
-  # activate :relative_assets
+#   activate :imageoptim do |options|
+#     options.manifest = true
+#     options.nice = true
+#     options.threads = true
+#     options.svgo = false
+#   end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
-
-  # github.com/yterajima/middleman-robots
-  # Make sure this stays on the bottom
   activate :robots, rules: [
     { user_agent: '*', allow: ['/'] }
   ],
