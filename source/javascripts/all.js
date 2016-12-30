2181ca6ab2f67b96dde8db0428f8dd014816bfa2
Miguel Palhas <mpalhas@gmail.com>
2016-12-30 19:54:24 +0000

    Removing old dependencies

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,12 +4,10 @@
 //= require picturefill
 //= require fastclick
 //= require gsap/src/minified/TweenMax.min
-//= require snap.svg.js
 //= require blue
 //= require _fastclick_init
 //= require _input
 //= require _animation_helpers
-//= require _custom_events
 //= require _blog_posts
 //= require company/_planets
 //= require company/_lines
