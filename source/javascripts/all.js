768fe54a29fe9550ce741d3417d7e18e5faf9438
Fernando Mendes <devfrmendes@gmail.com>
2017-01-26 15:21:38 +0000

    Adds Full Story

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,6 +6,7 @@
 //= require _fastclick_init
 //= require _input
 //= require _blog_posts
+//= require _full_story
 //= require gsap/src/uncompressed/TweenLite
 //= require nav/_logo
 //= require _segment
