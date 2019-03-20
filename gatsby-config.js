db0bc02818e86552833771987269d46144cf66d4
Pedro Costa <dev@iampfac.com>
2019-03-20 23:16:22 +0000

    Render team members from data file

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,13 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/data`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
