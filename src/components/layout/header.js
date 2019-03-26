b859006c7e5aa7455bcd55658f608c1bdabcd92c
Pedro Costa <dev@iampfac.com>
2019-03-26 17:58:05 +0000

    Header component structure

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,12 +2,29 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./header.css"
+import logo from "../../images/subvisual-logo-blue.svg"
+
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">{siteTitle}</Link>
-      </h1>
+  <header className="Header">
+    <div className="Header-content">
+      <div className="Header-left">
+        <Link to="/">
+          <img src={logo} alt="Subvisual" className="Header-logo" />
+        </Link>
+      </div>
+      <div className="Header-right">
+        <nav>
+          <ul className="Header-links">
+            <li className="Header-link">
+              <Link to="#">Blog</Link>
+            </li>
+            <li className="Header-link">
+              <Link to="#">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 )
