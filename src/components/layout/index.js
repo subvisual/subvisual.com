462220ac78899e6f1e2445d2e87e2ea0c37b77c7
Pedro Costa <dev@iampfac.com>
2019-03-26 15:14:18 +0000

    Remove Gatsby generated inline layout styles

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -20,17 +20,8 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <Footer />
-        </div>
+        <main>{children}</main>
+        <Footer />
       </>
     )}
   />
