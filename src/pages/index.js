0fe225c139b79650d5bd41cbba4428e0a5907983
Pedro Costa <dev@iampfac.com>
2019-03-27 12:37:35 +0000

    Add Hero structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Hero from "../components/home/hero"
 import Layout from "../components/layout"
-import PageTitle from "../components/page_title"
 import SEO from "../components/seo"
 import Team from "../components/team"
 
@@ -14,13 +14,7 @@ const IndexPage = () => (
       title="Subvisual - We nurture ideas that empower people"
       keywords={[]}
     />
-    <PageTitle>We nurture ideas that empower people</PageTitle>
-    <p>
-      Building a company from the ground up is hard and stressful, particularly
-      in heavy regulated fields such as fintech and digital healthcare.
-      Struggling with designing and developing that awesome idea you’ve had? We
-      can help you succeed. Together.
-    </p>
+    <Hero />
     <h2>Meaningful Ventures</h2>
     <h3>Looking for</h3>
     <dl>
@@ -57,33 +51,33 @@ const IndexPage = () => (
     </dl>
     <p>
       Join our venture universe.
-      <br />
+        <br />
       Let's talk
-    </p>
+      </p>
     <Link to="#">Join our universe</Link>
     <h2>About</h2>
     <h3>It started out of friendship</h3>
     <p>
       Subvisual was born out of friendship, with the ambition to become a
       company we'd love to work in.
-    </p>
+      </p>
     <p>
       By putting people first and never compromising on quality, we were able to
       shape a team culture that will embrace you and nurture your ideas to
       fruition.
-    </p>
+      </p>
     <h3>And grew with craft</h3>
     <p>
       We're a small, but powerful team, that can help you make informed
       decisions throughout the process of building a digital product.
-    </p>
+      </p>
     <p>
       We'll challenge you to think further and help you do the heavy lifting of
       shaping your product's development.
-    </p>
+      </p>
     <h3>Team</h3>
     <Team />
-  </Layout>
+  </Layout >
 )
 
 export default IndexPage
