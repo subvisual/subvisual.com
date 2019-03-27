8bdc7aecedb7580caad6093663bc09f1893dc11b
Pedro Costa <dev@iampfac.com>
2019-03-27 19:17:49 +0000

    Add Text component

--- src/components/call_to_action/index.js
+++ src/components/call_to_action/index.js
@@ -1,12 +1,14 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import classNames from "classnames"
 
 import "./index.css"
 
-const CallToAction = ({ children, white }) => {
+const CallToAction = ({ children, white, large }) => {
   const className = classNames({
     CallToAction: true,
+    "CallToAction--large": large,
     "CallToAction--white": white,
   })
 
@@ -15,6 +17,16 @@ const CallToAction = ({ children, white }) => {
       {children}
     </Link>
   )
+}
+
+CallToAction.propTypes = {
+  large: PropTypes.bool,
+  white: PropTypes.bool,
+}
+
+CallToAction.defaultProps = {
+  large: false,
+  white: false,
 }
 
 export default CallToAction
