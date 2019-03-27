f032a41981488d9563a2c543ed22920b57d2758b
Pedro Costa <dev@iampfac.com>
2019-03-27 16:22:34 +0000

    Add Colfax through Typekit

--- src/components/page_title/index.js
+++ src/components/page_title/index.js
@@ -2,8 +2,6 @@ import React from "react"
 
 import "./index.css"
 
-const PageTitle = ({ children }) => (
-  <h1 class="PageTitle">{children}</h1>
-)
+const PageTitle = ({ children }) => <h1 className="PageTitle">{children}</h1>
 
 export default PageTitle
