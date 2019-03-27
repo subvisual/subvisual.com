f42bd9ea9a0bdd4cdd9836227f8e5f1b2d56be31
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2019-03-27 19:30:40 +0000

    Use gatsby-image in Hero and enable CSSModules

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,22 @@ module.exports = {
         icon: `src/images/subvisual-symbol-blue.svg`,
       },
     },
+    {
+      resolve: `gatsby-plugin-react-css-modules`,
+      options: {
+        // *.css files are included by default.
+        // To support another syntax (e.g. SCSS),
+        // add `postcss-scss` to your project's devDependencies
+        // and add the following option here:
+        filetypes: {
+          ".scss": { syntax: `postcss-scss` },
+        },
+
+        // Exclude global styles from the plugin using a RegExp:
+        exclude: `\/global\/`,
+        // For all the options check babel-plugin-react-css-modules README link provided above
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
