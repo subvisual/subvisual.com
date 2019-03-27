f7a61498465cc49f0b50674cf865cd99fbd57eb2
Pedro Costa <dev@iampfac.com>
2019-03-27 16:55:21 +0000

    Structure the ventures section

---
+++ src/components/home/ventures_section.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "./ventures_section.css"
+
+const VenturesSection = () => (
+  <section className="VenturesSection">
+    <div className="VenturesSection-content">
+      <header className="VenturesSection-header">
+        <h2>Meaningful Ventures</h2>
+      </header>
+      <section className="VenturesSection-section">
+        <h3>Looking for</h3>
+        <dl>
+          <dt>Product Vision</dt>
+          <dd>Ideas that solve a clear problem for real users</dd>
+          <dt>Business Strategy</dt>
+          <dd>With clear goals, aiming for sustainable growth</dd>
+          <dt>Founders & Culture</dt>
+          <dd>Backed by committed people, with the right values</dd>
+        </dl>
+      </section>
+      <section className="VenturesSection-section">
+        <h3>Helping with</h3>
+        <ul>
+          <li>Seed investment & incubation</li>
+          <li>Strong company culture</li>
+          <li>Early stage product mentorship</li>
+          <li>Strong international ecosystem</li>
+        </ul>
+      </section>
+      <section className="VenturesSection-section">
+        <h3>Portfolio</h3>
+        <dl>
+          <dt>UTRUST</dt>
+          <dd>Crypto payments, simple as they should be</dd>
+          <dt>Keyruptive</dt>
+          <dd>Security matches convenience</dd>
+          <dt>siosLIFE</dt>
+          <dd>For younger spirits</dd>
+          <dt>Crediflux</dt>
+          <dd>Credit analysis: Simple, fase, and paperless</dd>
+          <dt>Oncostats</dt>
+          <dd>Defeating a common enemy, cancer</dd>
+          <dt>SV Health</dt>
+          <dd>Nurturing digital healthcare solutions</dd>
+          <dt>Finiam</dt>
+          <dd>Active digital finance</dd>
+        </dl>
+      </section>
+      <footer className="VenturesSection-footer">
+        <p>
+          Join our venture universe.
+          <br />
+          Let's talk
+        </p>
+        <Link to="#">Join our universe</Link>
+      </footer>
+    </div>
+  </section>
+)
+
+export default VenturesSection
