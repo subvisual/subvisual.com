1b83e8d7128458f799c6046ee57fdc62cfcb84e3
Pedro Costa <dev@iampfac.com>
2019-03-29 16:38:24 +0000

    Add Intro animation

---
+++ src/components/home/Intro/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Logo from "../../Logo"
+
+import "./index.module.css"
+
+const Intro = () => (
+  <div styleName="root">
+    <div styleName="logo">
+      <Logo color="darkBlue" />
+    </div>
+  </div>
+)
+
+export default Intro
