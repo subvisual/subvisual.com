ddd3b1f01f85f92619d2b85b86be2ba1e38197c4
Pedro Costa <dev@iampfac.com>
2019-03-29 00:00:55 +0000

    Change team member component to use text component

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -3,16 +3,21 @@ import PropTypes from "prop-types"
 
 import "./index.module.css"
 
-const Text = ({ color, children, size }) => (
-  <span styleName={`root ${size} ${color}`}>{children}</span>
-)
+const Text = ({ bold, color, children, size }) => {
+  const styleNames = ["root", bold ? "bold" : false, color, size]
+  const styleName = styleNames.filter(Boolean).join(" ")
+
+  return <span styleName={styleName}>{children}</span>
+}
 
 Text.propTypes = {
+  bold: PropTypes.bool,
   color: PropTypes.string,
   size: PropTypes.string,
 }
 
 Text.defaultProps = {
+  bold: false,
   color: "black",
   size: "regular",
 }
