c860e8c255390a87872591f462a229dddcda9b2a
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2019-03-29 11:22:29 +0000

    Add ventures with pictures (#189)

---
+++ src/components/home/VenturesPhotos/index.js
@@ -0,0 +1,58 @@
+import React from "react"
+import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
+
+import "./index.module.css"
+
+const VenturesPhotos = props => {
+  return (
+    <ul styleName="root">
+      {props.ventures.map(({ name, description, cover }) => (
+        <li styleName="item" key={name}>
+          <Img
+            {...cover.childImageSharp}
+            imgStyle={{ display: "block" }}
+            alt={name}
+          />
+          <div styleName="details">
+            {name}
+            <br /> {description}
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const query = graphql`
+  query {
+    allVenturesYaml {
+      edges {
+        node {
+          name
+          description
+          cover {
+            childImageSharp {
+              fluid(maxHeight: 452, quality: 100) {
+                ...GatsbyImageSharpFluid_noBase64
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default () => (
+  <StaticQuery
+    query={query}
+    render={data => {
+      return (
+        <VenturesPhotos
+          ventures={data.allVenturesYaml.edges.map(e => e.node)}
+        />
+      )
+    }}
+  />
+)
