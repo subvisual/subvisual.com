04d1b6ece29ab9f996f0994dbf3d9c8dfe50b6bd
Pedro Costa <dev@iampfac.com>
2019-03-29 13:08:37 +0000

    Style the Helping With section

---
+++ src/components/home/Ventures/HelpingWith/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./index.module.css"
+
+const HelpingWith = () => (
+  <ul styleName="root">
+    <li styleName="item">Seed investment & incubation</li>
+    <li styleName="item">Strong company culture</li>
+    <li styleName="item">Early stage product mentorship</li>
+    <li styleName="item">Strong international ecosystem</li>
+  </ul>
+)
+
+export default HelpingWith
