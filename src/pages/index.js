51bd05482c821ecd7504d77961fb8cc5eb2ef76f
Pedro Costa <dev@iampfac.com>
2019-03-29 16:38:24 +0000

    Add Intro animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Hero from "../components/home/Hero"
+import Intro from "../components/home/Intro"
 import About from "../components/home/About"
 import Ventures from "../components/home/Ventures"
 import Layout from "../components/layout"
@@ -9,15 +10,18 @@ import SEO from "../components/seo"
 import "../common/normalize.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Subvisual - We nurture ideas that empower people"
-      keywords={[]}
-    />
-    <Hero />
-    <Ventures />
-    <About />
-  </Layout>
+  <>
+    <Intro />
+    <Layout>
+      <SEO
+        title="Subvisual - We nurture ideas that empower people"
+        keywords={[]}
+      />
+      <Hero />
+      <Ventures />
+      <About />
+    </Layout>
+  </>
 )
 
 export default IndexPage
