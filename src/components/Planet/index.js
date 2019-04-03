640b10c0d9edf00e4c328dedd5871482b30911a5
Pedro Costa <dev@iampfac.com>
2019-04-03 20:10:47 +0100

    Fix Planet animations
    Why:
    
    * Fuck Chrome.

--- src/components/Planet/index.js
+++ src/components/Planet/index.js
@@ -1,6 +1,10 @@
 import _ from "lodash"
 import React, { Component } from "react"
 import PropTypes from "prop-types"
+import Color from "color"
+import uuid from "uuid/v4"
+
+import colorCodes from "../colors"
 
 import "./index.module.css"
 
@@ -9,24 +13,66 @@ class Planet extends Component {
     super(props)
 
     this.hoveringAmplitude = _.random(-10, 10)
-    this.hoveringDelay = _.random(0, 3)
-    this.hoveringDuration = _.random(0.8, 1.8)
+    this.hoveringAnimationName = `planet-${uuid()}-hovering`
+    this.hoveringAnimationDelay = _.random(0, 3)
+    this.hoveringAnimationDuration = _.round(_.random(0.8, 1.8), 2)
+  }
+
+  get radialGradient() {
+    const { color, size } = this.props
+    const radius = Math.round(size * 0.83)
+    const end = colorCodes[color]
+    const start = Color(end)
+      .fade(0.4)
+      .rgb()
+      .toString()
+
+    return `radial-gradient(${radius}px at 83% 15%, ${start} 0%, ${end} 100%)`
+  }
+
+  get rootStyle() {
+    const { hovering, size } = this.props
+    const style = {
+      width: `${size}px`,
+      height: `${size}px`,
+    }
+
+    if (!hovering) return style
+
+    return {
+      ...style,
+      animationName: this.hoveringAnimationName,
+      nimationDelay: `${this.hoveringAnimationDelay}s`,
+      animationDuration: `${this.hoveringAnimationDuration}s`,
+      animationTimingFunction: "ease-in-out",
+      animationDirection: "alternate",
+      animationIterationCount: "infinite",
+    }
   }
 
   render() {
-    const { color, hovering } = this.props
-    const style = {
-      "--hovering-translate-y-end": this.hoveringAmplitude,
-      "--hovering-delay": this.hoveringDelay,
-      "--hovering-duration": this.hoveringDuration,
-      "--size": this.props.size,
-    }
-
     return (
-      <div
-        style={style}
-        styleName={`root ${color} ${hovering ? "hovering" : ""}`}
-      />
+      <>
+        <style>
+          {`@keyframes ${this.hoveringAnimationName} {
+            0% {
+              transform: translateY(0);
+              -moz-transform: translateY(0)) rotate(-0.01deg);
+            }
+
+            100% {
+              transform: translateY(${this.hoveringAmplitude}px);
+              -moz-transform: translateY(${
+                this.hoveringAmplitude
+              }px) rotate(0.01deg);
+            }
+          }`}
+        </style>
+        <div style={this.rootStyle} styleName="root">
+          <div styleName="background" />
+          <div styleName="planet" style={{ background: this.radialGradient }} />
+        </div>
+      </>
     )
   }
 }
