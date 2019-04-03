85c78972576b5166ba80b165c66270495c6610d6
Pedro Costa <dev@iampfac.com>
2019-04-03 16:08:54 +0100

    Planets now hover with a random amplitude

--- src/components/Planet/index.js
+++ src/components/Planet/index.js
@@ -1,14 +1,37 @@
-import React from "react"
+import _ from "lodash"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 
 import "./index.module.css"
 
-const Planet = ({ color, hovering, size }) => (
-  <div
-    style={{ transform: `scale(${size / 100})` }}
-    styleName={`root ${color} ${hovering ? "hovering" : ""}`}
-  />
-)
+class Planet extends Component {
+  constructor(props) {
+    super(props)
+
+    this.hoveringAmplitude = _.random(-10, 10)
+    console.log(this, this.hoveringAmplitude)
+  }
+
+  componentDidMount() {
+    this.container.style.setProperty(
+      "--hovering-translate-y-end",
+      this.hoveringAmplitude
+    )
+  }
+
+  render() {
+    const { color, hovering, size } = this.props
+    const hoveringAmplitude = this.hoveringAmplitude
+
+    return (
+      <div
+        ref={el => (this.container = el)}
+        style={{ transform: `scale(${size / 100})` }}
+        styleName={`root ${color} ${hovering ? "hovering" : ""}`}
+      />
+    )
+  }
+}
 
 Planet.propTypes = {
   color: PropTypes.string.isRequired,
