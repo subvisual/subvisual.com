9cb26dc14c6d753f74c5e89cec1c18db47261894
Pedro Costa <dev@iampfac.com>
2019-04-04 17:57:26 +0100

    Cleanup Call to Action component

--- src/components/CallToAction/index.js
+++ src/components/CallToAction/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import { Link } from "gatsby"
-
 import "./index.module.css"
 
 const CallToAction = ({ color, children, size }) => (
