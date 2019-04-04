c7c9d50aad504342a707a7a355963538eeed2862
Pedro Costa <dev@iampfac.com>
2019-04-04 04:19:24 +0100

    Remove animations
    Why:
    
    * Fuck everything

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,4 +1,3 @@
-import "intersection-observer"
 import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { useMorph } from "react-morph"
-import { easeOutSin, easeInSin } from "react-morph/lib/easings"
 
 import About from "../components/home/About"
 import Hero from "../components/home/Hero"
@@ -10,22 +8,7 @@ import Ventures from "../components/home/Ventures"
 
 import "../common/normalize.css"
 
-const easings = {
-  translateX: easeOutSin,
-  translateY: easeInSin,
-}
-
-const spring = {
-  damping: 10,
-  mass: 1,
-  stiffness: 40,
-}
-
-const config = { easings, spring, getMargins: true }
-
 const IndexPage = () => {
-  const morph = useMorph(config)
-
   return (
     <>
       <Layout>
@@ -33,9 +16,9 @@ const IndexPage = () => {
           title="Subvisual - We nurture ideas that empower people"
           keywords={[]}
         />
-        <Hero planetMorph={morph} />
-        <Ventures planetMorph={morph} />
-        <About planetMorph={morph} />
+        <Hero />
+        <Ventures />
+        <About />
       </Layout>
     </>
   )
