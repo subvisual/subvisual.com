64dace7072b2cbce6efec72a8cf0d2be4f20d4f3
Pedro Costa <dev@iampfac.com>
2019-04-04 04:21:51 +0100

    Bring back the intersection observer polyfill
    Why:
    
    * Apparently we need it. Ask Justo.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+exports.onInitialClientRender(() => {
+  require("intersection-observer")
+})
