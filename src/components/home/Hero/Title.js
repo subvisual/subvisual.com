cb4fa4bd511ed014e710641bbc3c08d83da930f8
José Sousa <josesousa9000@gmail.com>
2019-04-12 15:17:40 +0100

    wip

--- src/components/home/Hero/Title.js
+++ src/components/home/Hero/Title.js
@@ -3,21 +3,35 @@ import PropTypes from "prop-types"
 
 import PageTitle from "../../PageTitle"
 import Planet from "../../Planet"
+import useDetectJavascript from "../../../common/useDetectJavascript"
 
 import "./Title.module.css"
 
-const Title = ({ planetMorph }) => (
-  <PageTitle>
-    We nurture{" "}
-    <span styleName="ideas">
-      ideas
-      <span styleName="planet">
-        <Planet morph={planetMorph} codeName="heroTittle" color="blue" />
-      </span>
-    </span>{" "}
-    <span styleName="glue">that empower</span> people
-  </PageTitle>
-)
+const Title = ({ planetMorph }) => {
+  const hasJavascript = useDetectJavascript()
+
+  if (hasJavascript) {
+    return (
+      <PageTitle>
+        We nurture{" "}
+        <span styleName="ideas">
+          ideas
+          <span styleName="planet">
+            <Planet morph={planetMorph} codeName="heroTittle" color="blue" />
+          </span>
+        </span>{" "}
+        <span styleName="glue">that empower</span> people
+      </PageTitle>
+    )
+  }
+
+  return (
+    <PageTitle regularFont>
+      We nurture <span styleName="ideas">ideas</span>{" "}
+      <span styleName="glue">that empower</span> people
+    </PageTitle>
+  )
+}
 
 Title.propTypes = {
   planetMorph: PropTypes.func,
