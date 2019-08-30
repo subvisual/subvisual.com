abc7c7ef077387e13e5d92f44f07a94ea8882178
Pedro Costa <dev@iampfac.com>
2019-08-30 18:33:13 +0100

    Refactor layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,13 +2,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 
-import Header from "./header"
-import Footer from "./footer"
+import Header from "./layout/header"
+import Footer from "./layout/footer"
 
-import "./index.module.css"
+import styles from "./layout.module.css"
 
 const Layout = ({ children }) => (
-  <div styleName="root">
+  <div className={styles.root}>
     <Helmet>
       <link rel="stylesheet" href="https://use.typekit.net/bcx8qfd.css" />
     </Helmet>
