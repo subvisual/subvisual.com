8de1434ee2f484f32332b1a4488cc193fd0e87df
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2020-01-31 17:34:13 +0000

    Optimize the imports of lodash (#226)
    Importing lodash usign `import _ from "lodash"` will import the whole
    library. The correct way to import lodash is to only import the pieces
    you need, for instance `import map from lodash/map`. In this case, we
    simply don't need lodash in this situation, so I removed it. It's still
    being used in other places, but this already removes around 70kb to the
    bundle size. And because this is JS and our initial animation
    requires JS to run, we can use as many optimizations as we can.

--- src/components/layout/footer/social_links.js
+++ src/components/layout/footer/social_links.js
@@ -1,4 +1,3 @@
-import _ from "lodash"
 import React from "react"
 
 import SocialLink from "../../SocialLink"
@@ -19,12 +18,12 @@ const socialDetails = {
 
 const SocialLinks = () => (
   <ul className={styles.root}>
-    {_.map(socialDetails, (url, platform) => (
+    {Object.keys(socialDetails).map(platform => (
       <li key={platform} className={styles.item}>
         <SocialLink
           name="Subvisual"
           platform={platform}
-          url={url}
+          url={socialDetails[platform]}
           size="small"
         />
       </li>
