a6bb48e83feb550df7c927ced10f7c3b1fcfb71e
Gabriel Poça <gabrielpoca@gmail.com>
2020-02-14 15:51:51 +0000

    hotfix: second attempt to fix aspect Ratio in images

--- src/components/home/about_section/team_subsection/member.js
+++ src/components/home/about_section/team_subsection/member.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import Img from "gatsby-image"
 
@@ -7,50 +7,51 @@ import Text from "../../../text"
 
 import styles from "./member.module.css"
 
-const Member = ({ name, role, social, photo }) => (
-  <div className={styles.root}>
-    <Img
-      fadeIn
-      fluid={[
-        {
-          ...photo.horizontal.childImageSharp.fluid,
-          aspectRatio: 1.6666666667,
-          media: "(max-width: 767px)",
-        },
-        {
-          ...photo.vertical.childImageSharp.fluid,
-          aspectRatio: 0.85625,
-          media: "(min-width: 768px)",
-        },
-      ]}
-    />
-    <div className={styles.info}>
-      <div className={styles.name}>
-        <Text color="white" bold>
-          {name}
-        </Text>
+const Member = ({ name, role, social, photo }) => {
+  return (
+    <div className={styles.root}>
+      <Img
+        className={styles.aspectRatioHack}
+        fadeIn
+        fluid={[
+          {
+            ...photo.horizontal.childImageSharp.fluid,
+            media: "only screen and (max-width: 768px)",
+          },
+          {
+            ...photo.vertical.childImageSharp.fluid,
+            media: "only screen and (min-width: 768px)",
+          },
+        ]}
+      />
+      <div className={styles.info}>
+        <div className={styles.name}>
+          <Text color="white" bold>
+            {name}
+          </Text>
+        </div>
+        <div className={styles.role}>
+          <Text color="white">{role}</Text>
+        </div>
       </div>
-      <div className={styles.role}>
-        <Text color="white">{role}</Text>
-      </div>
-    </div>
-    <ul aria-label="Social Links" className={styles.links}>
-      {Object.keys(social).map(platform => {
-        if (!social[platform]) return null
+      <ul aria-label="Social Links" className={styles.links}>
+        {Object.keys(social).map(platform => {
+          if (!social[platform]) return null
 
-        return (
-          <li key={platform} className={styles.link}>
-            <SocialLink
-              name={name}
-              platform={platform}
-              url={social[platform]}
-            />
-          </li>
-        )
-      })}
-    </ul>
-  </div>
-)
+          return (
+            <li key={platform} className={styles.link}>
+              <SocialLink
+                name={name}
+                platform={platform}
+                url={social[platform]}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
 
 Member.propTypes = {
   name: PropTypes.string.isRequired,
