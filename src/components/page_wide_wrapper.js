4815e0df43291e63be5cac4961899366b96dde39
Pedro Costa <pfac@users.noreply.github.com>
2020-03-20 17:26:26 +0000

    Add wrapper components for the blog posts (#253)
    Why:
    
    * The layout of the blog is a tricky one with 3 levels.
    
      On the outer the content is centered with a limit to the maximum width
      of the design. This may or may not be padded (for instance images have
      to fill the entire width without side padding in mobile).
    
      Below that level is the post content, including header and body, which
      has a smaller limit width and is aligned to the left inside the outer
      container.
    
      Lastly, there's the body of the post, which is aligned to the right
      within the post container, also with a smaller width.
    
      These containers allow the layout to responsively adapt to the entire
      range of widths from mobile to desktop, only causing a jump when
      font sizes and padding sizes have to change.

---
+++ src/components/page_wide_wrapper.js
@@ -0,0 +1,28 @@
+import React from "react"
+import PropTypes from "prop-types"
+import classNames from "classnames"
+
+import styles from "./page_wide_wrapper.module.css"
+
+const PageWideWrapper = ({ children, padded }) => {
+  const className = classNames(styles.root, {
+    [styles.padded]: padded,
+  })
+
+  return (
+    <div className={className}>
+      <div className={styles.content}>{children}</div>
+    </div>
+  )
+}
+
+PageWideWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+  padded: PropTypes.bool,
+}
+
+PageWideWrapper.defaultProps = {
+  padded: false,
+}
+
+export default PageWideWrapper
