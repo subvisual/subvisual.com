8928f4783fd8676134d0d7a155de395bfb482109
Pedro Costa <dev@iampfac.com>
2020-03-27 17:04:36 +0000

    Replaces postcss-nested with Gatsby SASS
    :radioactive: don't `node-sass` open inside
    
    Why:
    
    * Trying to cleanup the post body styles in #212, the best approach I
      found was to use SASS to import styles directly in the middle of other
      files. I looked into `precss`, but turns out there is a recommended
      Gatsby plugin that uses Node SASS underneath, giving us a more battle
      tested support for SASS. I also tried `precss` and wasn't able to
      achieve quite what I had in mind, but it worked with the Gatsby SASS
      plugin. :shrug:

--- src/components/call_to_action.js
+++ src/components/call_to_action.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import styles from "./call_to_action.module.css"
+import styles from "./call_to_action.module.scss"
 
 const CallToAction = ({ color, children, size }) => {
   const className = [styles.root, styles[color], styles[size]].join(" ")
