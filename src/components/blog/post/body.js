2a015cd7426cc94fbdaa5c8e55c4ec2bf5cbe234
Pedro Costa <pfac@users.noreply.github.com>
2020-03-28 13:54:04 +0000

    Add blog post body component (#257)
    Why:
    
    * The body of a blog post will be generated from the respective Markdown
      file. As such, we need to style the generated elements without being
      able to set specific classes to each one.
    * There are also many details here that have to tweaked all at once
      since we can't split the inner parts of the Markdown document into
      smaller components, such as spacings, typography, adjustments for
      responsiveness, etc.

---
+++ src/components/blog/post/body.js
@@ -0,0 +1,19 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+import BodyWrapper from "./body_wrapper"
+
+import styles from "./body.module.scss"
+
+const PostBody = ({ html }) => (
+  <BodyWrapper>
+    {/* eslint-disable-next-line react/no-danger */}
+    <div className={styles.root} dangerouslySetInnerHTML={{ __html: html }} />
+  </BodyWrapper>
+)
+
+PostBody.propTypes = {
+  html: PropTypes.string.isRequired,
+}
+
+export default PostBody
