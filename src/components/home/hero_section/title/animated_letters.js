2d23c5613d8e91b3e95624b415703215af6f73fa
Pedro Costa <dev@iampfac.com>
2020-04-05 15:00:54 +0100

    Refactor hero title component
    Why:
    
    * The logic to animate the hero title is mixed with the rest of the
      component, which makes it next to impossible to disable animations.
    * When the hero tittle is the planet hovering, we're using a different
      unicode character to print an `i` without the tittle. However this
      messes the content. For instance, if you copy and paste the title, the
      character copied is not an `i`.
    * Despite using a different character, we're using a different font
      family for the tittleless `i`, which is Base64 encoded in the fonts
      stylesheet.
    
    This change addresses these issues by:
    
    * Extracting the animation logic in the hero section title component
      into its own sub-component.
    * Refactoring the content of the title to be properly copyable, and use
      a proper `i`.
    * Refactoring the planet component to remove the `hide` property,
      instead the component using it decides whether or not to render it.
    * Replacing the Base64 encoded subset font with one containing only the
      `i` but without the tittle in the glyph.

---
+++ src/components/home/hero_section/title/animated_letters.js
@@ -0,0 +1,89 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { motion } from "framer-motion"
+
+import Planet from "../../../planet"
+import useDetectJavascript from "../../../../utils/use_detect_javascript"
+
+import styles from "./animated_letters.module.scss"
+
+const dragVariants = {
+  in: {
+    opacity: 1,
+    transition: {
+      ease: "easeIn",
+      delay: 1.5,
+      delayChildren: 1.5,
+      staggerChildren: 0.04,
+    },
+  },
+  out: {
+    opacity: 0,
+  },
+}
+
+const letterVariants = {
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+}
+
+const renderAnimatedLetters = string =>
+  Array.from(string).map((letter, index) => (
+    // eslint-disable-next-line react/no-array-index-key
+    <motion.span key={index} variants={letterVariants}>
+      {letter}
+    </motion.span>
+  ))
+
+const renderPlanet = ({ hidden, morph }) => {
+  if (hidden) return null
+
+  const planet = (
+    <Planet
+      morph={morph}
+      color="blue"
+      hoverAnimation={{
+        delay: 4.8,
+        duration: 1.5,
+        endYAt: -4,
+      }}
+      hovering
+    />
+  )
+
+  return planet
+}
+
+const HeroTitleAnimatedLetters = ({ hidden, planetMorph }) => (
+  <motion.span
+    variants={dragVariants}
+    animate={hidden ? "out" : "in"}
+    key="without-files"
+  >
+    {renderAnimatedLetters("We nurture ")}
+    <span className={styles.ideas}>
+      <motion.span className={styles.tittleless} variants={letterVariants}>
+        i
+      </motion.span>
+      <span className={styles.planet}>
+        {renderPlanet({ hidden, morph: planetMorph })}
+      </span>
+      {renderAnimatedLetters("deas")}
+    </span>
+    <span className={styles.glue}>
+      {renderAnimatedLetters(" that empower ")}
+    </span>
+    {renderAnimatedLetters("people")}
+  </motion.span>
+)
+
+HeroTitleAnimatedLetters.propTypes = {
+  hidden: PropTypes.bool.isRequired,
+  planetMorph: PropTypes.func.isRequired,
+}
+
+export default HeroTitleAnimatedLetters
