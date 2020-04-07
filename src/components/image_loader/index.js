12b706db39586bd5de9cd8ed69dd7e1e6842d237
Pedro Costa <dev@iampfac.com>
2020-04-07 10:53:00 +0100

    Remove image placeholders on JS disabled
    Why:
    
    * If Javascript is disabled all images loaded lazily are not shown
      because the placeholder never leaves.

--- src/components/image_loader/index.js
+++ src/components/image_loader/index.js
@@ -3,9 +3,13 @@ import { useInView } from "react-intersection-observer"
 import Img from "gatsby-image"
 
 import Placeholder from "../load_placeholder/placeholder"
+import useDetectJavascript from "../../utils/use_detect_javascript"
+
 import styles from "./index.module.css"
 
 export default props => {
+  const hasJavascript = useDetectJavascript()
+
   const { delay, darkOverlay, imgStyle, ...imgProps } = props
   const [loaded, setLoaded] = React.useState(false)
   const [ref, inView] = useInView({
@@ -16,7 +20,7 @@ export default props => {
   return (
     <div ref={ref} className={styles.root}>
       <Placeholder
-        visible={inView && loaded}
+        visible={hasJavascript && inView && loaded}
         delay={delay}
         dark={darkOverlay}
       />
