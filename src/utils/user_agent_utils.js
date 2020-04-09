b4b1279f459a2a57871ebf88d5cf373226fbcaf1
Pedro Costa <dev@iampfac.com>
2020-04-09 22:57:18 +0100

    Fix drop cap vertical position
    Why:
    
    * We increased the line height of the text body which caused the drop
      cap to be slightly misaligned.
    
    This change addresses the issue by:
    
    * Manually tweaking the position for all browsers to get pixel perfect
      positioning when we have JavaScript in the browser.

--- src/utils/user_agent_utils.js
+++ src/utils/user_agent_utils.js
@@ -1,3 +1,8 @@
 export const isLinux = ({ os: { name } }) => name === "Linux"
+export const isMacOS = ({ os: { name } }) => name === "Mac OS"
 
-export default { isLinux }
+export const isChrome = ({ browser: { name } }) => name === "Chrome"
+export const isEdge = ({ browser: { name } }) => name === "Edge"
+export const isSafari = ({ browser: { name } }) => name === "Safari"
+
+export default { isLinux, isSafari }
