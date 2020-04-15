fde7e175d4c39e8cad33a197789a7cacf763df53
Miguel Palhas <mpalhas@gmail.com>
2020-04-15 18:22:42 +0100

    Moves posts to src/posts
    Post file names no longer reflect the URL they live under, so it's a bit
    less useful to have them nested under `src/pages`.
    
    Here, we move directly them to `src/posts`
    ```

--- config/node/blog_posts.js
+++ config/node/blog_posts.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
-const relativePath = "src/pages/blog/posts"
+const relativePath = "src/posts"
 const absolutePath = path.join(__dirname, "../..", relativePath)
 
 module.exports = { absolutePath, relativePath }
