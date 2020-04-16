0e3fb3c0a386afcca1806194e1d484104b8a0c35
Pedro Costa <pfac@users.noreply.github.com>
2020-04-16 12:21:00 +0100

    Make blog post title the blog default title (#308)
    Why:
    
    * We'll be using it in other blog pages, like the authors page in
      preparation in #297.

--- src/components/blog/post/header.js
+++ src/components/blog/post/header.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import dateFormat from "dateformat"
 
-import Title from "./title"
+import Title from "../title"
 import Wrapper from "./wrapper"
 
 import styles from "./header.module.css"
---
+++ src/components/blog/title.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+import classNames from "classnames"
+
+import styles from "./title.module.css"
+
+const BlogTitle = ({ children, className }) => (
+  <h1 className={classNames(styles.root, className)}>{children}</h1>
+)
+
+BlogTitle.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+}
+
+export default BlogTitle
