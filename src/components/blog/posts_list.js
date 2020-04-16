afb58466a5750b638a24060b22a6f21d38975685
Pedro Costa <pfac@users.noreply.github.com>
2020-04-16 13:53:57 +0100

    Make posts list component page agnostic (#309)
    Why:
    
    * The posts list component is currently bound to the blog root page, and
      it is currently the one querying the data to include in this page;
    * We'll need to use this component in other pages, like the authors page
      in preparation in #297.
    
    This change addresses the need by:
    
    * Extracting the data query to the root blog page component;
    * Extracting any layout styling external to the posts list itself to the
      root blog page component;
    * Refactoring the posts list component to simply render the content
      based on a list of posts provided as a property;
    * Refactoring the posts list entry component to extract the author
      information provided as a single object.

--- src/components/blog/posts_list.js
+++ src/components/blog/posts_list.js
@@ -1,59 +1,31 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import PropTypes from "prop-types"
 
 import Entry from "./posts_list/entry"
 
 import styles from "./posts_list.module.scss"
 
-const query = graphql`
-  {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      nodes {
-        frontmatter {
-          author {
-            name
-          }
-          date
-          id
-          path
-          title
-          intro
-        }
-      }
-    }
-  }
-`
+const renderItem = ({ author, date, intro, id, path, title }) => (
+  <li key={id} className={styles.item}>
+    <Entry {...{ author, date, intro, path, title }} />
+  </li>
+)
 
-const renderItem = ({ frontmatter }) => {
-  const { author, date, id, path, ...entry } = frontmatter
-  const { name: authorName } = author
+const BlogPostsList = ({ posts }) => (
+  <ol className={styles.root}>{posts.map(renderItem)}</ol>
+)
 
-  const entryProps = {
-    author: authorName,
-    path,
-    date: new Date(date),
-    ...entry,
-  }
-
-  return (
-    <li key={id} className={styles.postsListItem}>
-      <Entry {...entryProps} />
-    </li>
-  )
-}
-
-const BlogPostsList = () => {
-  const {
-    allMarkdownRemark: { nodes: postsNodes },
-  } = useStaticQuery(query)
-
-  return (
-    <div className={styles.root}>
-      <div className={styles.content}>
-        <ol className={styles.postsList}>{postsNodes.map(renderItem)}</ol>
-      </div>
-    </div>
-  )
+BlogPostsList.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      author: PropTypes.object,
+      date: PropTypes.instanceOf(Date).isRequired,
+      id: PropTypes.number.isRequired,
+      intro: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 }
 
 export default BlogPostsList
