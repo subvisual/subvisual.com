6588d10155b25e7e857286f16c3bbd16f1a6464f
Pedro Costa <dev@iampfac.com>
2020-07-11 15:42:45 +0100

    wip: use useRef

--- src/components/AnimatedPlanet/Stage.jsx
+++ src/components/AnimatedPlanet/Stage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import PropTypes from "prop-types"
 
 import Planet from "./Planet"
@@ -6,24 +6,19 @@ import useWindowSize from "../../utils/use_window_size"
 import variants, { transition } from "./variants"
 
 const Stage = ({ render }) => {
-  const [spikes, setSpikes] = useState([])
+  const spikes = [useRef()]
   const [current, setCurrent] = useState(0) // index of the current spike
   const windowSize = useWindowSize()
 
   const [heroTittle] = spikes
 
-  const spikeSetters = Array.from({ length: 1 }, (_, index) => (elem) => {
-    if (!elem || elem === spikes[index]) return
-
-    setSpikes([...spikes.slice(0, index), elem, ...spikes.slice(index + 1)])
-  })
   const actions = {
-    play: (spikeSetter) => setCurrent(spikeSetters.indexOf(spikeSetter) + 1)
+    play: (spike) => setCurrent(spikes.indexOf(spike) + 1),
   }
 
   return (
     <>
-      {render({ actions, spikes: spikeSetters })}
+      {render({ actions, spikes })}
       <Planet
         initial={variants[0].planet({ windowSize })}
         animate={variants[current].planet({
