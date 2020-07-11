31301a6ed311284af4ee6da080d41f127405b1e9
Pedro Costa <dev@iampfac.com>
2020-07-11 18:55:15 +0100

    wip

--- src/components/AnimatedPlanet/Stage.jsx
+++ src/components/AnimatedPlanet/Stage.jsx
@@ -6,33 +6,33 @@ import useWindowSize from "../../utils/use_window_size"
 import variants from "./variants"
 
 const Stage = ({ render }) => {
-  const spikes = [useRef()] // one reference for each spike
-  const [current, setCurrent] = useState() // index of the current spike
+  const spikes = [undefined, useRef()] // one reference for each spike
+  const [current, setCurrent] = useState(0) // index of the current spike
   const windowSize = useWindowSize()
 
-  const [heroTittle] = spikes
-
   const actions = {
-    play: (spike) => setCurrent(spikes.indexOf(spike) + 1),
+    play: (spike) => setCurrent(spikes.indexOf(spike)),
   }
+  const currentSpike = spikes[current]
+  const currentVariant = variants[current]
 
   return (
     <>
-      {render({ actions, spikes })}
+      {render({ actions, spikes: spikes.slice(1) })}
       <Planet
         initial={variants[0].planet({
           windowSize,
         })}
-        animate={variants[current || 0].planet({
-          spike: heroTittle.current,
+        animate={currentVariant.planet({
+          spike: currentSpike?.current,
           windowSize,
         })}
-        transition={variants[current || 0].transition}
+        transition={currentVariant.transition}
       >
         <Planet.Background
           initial={variants[0].background}
-          animate={variants[current || 0].background}
-          transition={variants[current || 0].transition}
+          animate={currentVariant.background}
+          transition={currentVariant.transition}
         />
       </Planet>
     </>
