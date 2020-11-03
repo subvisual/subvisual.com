e2ba384f6a9c94e3aef7308541e0b1c638137bba
Pedro Costa <dev@iampfac.com>
2020-11-03 17:26:11 +0000

    Add Norte 2020 links in footer
    Why:
    
    * https://3.basecamp.com/4319812/buckets/19267320/todos/3138565689
    
      We now have a second document we have to link regarding Norte 2020
      programs in the footer, but it's the same set of logos. Therefore we
      decided to simply list the documents after the logo bar.
    
    Note: PDF documents were optimized for a reduced file size.

---
+++ src/components/layout/footer/Norte2020/index.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import Link from "~/src/components/link"
+
+import styles from "./index.module.scss"
+
+const render = ({ logo, feder020919, fse000130 }) => (
+  <div className={styles.root}>
+    <div className={styles.image}>
+      <Img fadeIn fluid={logo.childImageSharp.fluid} />
+    </div>
+    <ol className={styles.links}>
+      <li className={styles.link}>
+        <Link to={feder020919.publicURL} download={feder020919.base} blank>
+          NORTE-02-0752-FEDER-020919
+        </Link>
+      </li>
+      <li className={styles.link}>
+        <Link to={fse000130.publicURL} download={fse000130.base} blank>
+          NORTE-06-3559-FSE-000130
+        </Link>
+      </li>
+    </ol>
+  </div>
+)
+
+const query = graphql`
+  {
+    logo: file(relativePath: { regex: "/norte-2020-logos.jpg/" }) {
+      childImageSharp {
+        fluid(maxWidth: 650, quality: 100) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+    feder020919: file(
+      relativePath: { regex: "/NORTE-02-0752-FEDER-020919.pdf$/" }
+    ) {
+      base
+      publicURL
+    }
+    fse000130: file(
+      relativePath: { regex: "/NORTE-06-3559-FSE-000130.pdf$/" }
+    ) {
+      base
+      publicURL
+    }
+  }
+`
+
+const Norte2020 = () => <StaticQuery query={query} render={render} />
+
+export default Norte2020
