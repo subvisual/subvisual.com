cdba36a54a13729c55d525bfdbf0fced5b0f6b91
Miguel Palhas <mpalhas@gmail.com>
2020-12-09 15:19:32 +0000

    Make SEO great again

--- src/components/SEO/MetaTags/OpenGraph/index.tsx
+++ src/components/SEO/MetaTags/OpenGraph/index.tsx
@@ -3,10 +3,11 @@ import Helmet from "react-helmet"
 
 import { Component } from "./types"
 
-const OpenGraph : Component = ({ description, image, title, url }) => (
+const OpenGraph: Component = ({ description, image, title, url }) => (
   <Helmet>
     <meta property="og:description" content={description} />
     <meta property="og:image" content={image} />
+    <meta property="og:image:secure_url" content={image} />
     <meta property="og:title" content={title} />
     <meta property="og:type" content="website" />
     <meta property="og:url" content={url} />
