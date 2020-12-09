027e948c4db91d34903375144ba60c26d5f92077
Miguel Palhas <mpalhas@gmail.com>
2020-12-09 15:37:07 +0000

    Make SEO great again (#418)
    * Make SEO great again

--- config/gatsby/siteMetadata.js
+++ config/gatsby/siteMetadata.js
@@ -2,7 +2,7 @@ const siteUrl = process.env.URL || "http://localhost:8000"
 
 module.exports = {
   description: "We nurture ideas that empower people",
-  image: new URL("/images/meta-image.jpg", siteUrl).toString(),
+  image: new URL("/images/meta.png", siteUrl).toString(),
   lang: "en",
   siteUrl,
   title: "Subvisual",
