d5516bcc9ca54756c265459047d7496c2458c10a
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2021-10-04 22:40:38 +0100

    Output the last blog posts to a JSON file (#458)
    This file will be used by the new website on Webflow.

--- config/gatsby/plugins.js
+++ config/gatsby/plugins.js
@@ -1,6 +1,10 @@
 const path = require("path")
+const normalizePathForRegExp = require("@subvisual/utils/normalizePathForRegExp")
 
 const ROOT = path.resolve(__dirname, "../..")
+const BLOG_POSTS_ABSOLUTE_PATH = path.resolve(ROOT, "src/posts")
+const siteUrl = process.env.URL || 'http://localhost:8000';
+const basePath = normalizePathForRegExp(BLOG_POSTS_ABSOLUTE_PATH)
 
 const sassConfig = require("./plugins/sass")(ROOT)
 const rssFeedConfig = require("./plugins/rssFeed")(ROOT)
@@ -113,4 +117,44 @@ module.exports = [
     },
   },
   `gatsby-plugin-netlify-cms`,
+  {
+    resolve: `gatsby-plugin-json-output`,
+    options: {
+      siteUrl,
+      graphQLQuery: `
+      {
+        allMarkdownRemark(
+          limit: 3,
+          filter: { fileAbsolutePath: { regex: "/^${basePath}/" } }
+          sort: {
+            fields: [frontmatter___date, frontmatter___title]
+            order: [DESC, DESC]
+          }
+        ) {
+          nodes {
+            frontmatter {
+              author {
+                key
+                name
+              }
+              date
+              path
+              title
+              intro
+            }
+          }
+        }
+      }
+      `,
+      serializeFeed: results => results.data.allMarkdownRemark.nodes.map(({ frontmatter }) => ({
+        id: frontmatter.path,
+        url: siteUrl + path.join('/blog', frontmatter.path),
+        title: frontmatter.title,
+        date: new Date(frontmatter.date).toISOString(),
+        author: frontmatter.author.name
+      })),
+      feedFilename: "blog/index",
+      nodesPerFeedFile: 100,
+    }
+  }
 ]
