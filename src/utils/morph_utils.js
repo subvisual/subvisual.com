5742fa903645508d268a371932e0e75ac80ada12
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2021-10-28 10:29:51 +0100

    Remove unecessary code now that we have a website in Webflow (#462)

--- src/utils/morph_utils.js
+++
@@ -1,8 +0,0 @@
-export function withMorphStyle(morph, otherStyle) {
-  const { style, ...restOfMorph } = morph
-  const blendedStyle = { ...otherStyle, ...style }
-
-  return [blendedStyle, restOfMorph]
-}
-
-export default { withMorphStyle }
