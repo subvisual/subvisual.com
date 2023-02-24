6b181da98415e998ca12f57e124122082f1388e4
Gabriel Poca <mail@gabrielpoca.com>
2023-02-24 14:11:42 +0000

    wip

--- config/gatsby/plugins.js
+++ config/gatsby/plugins.js
@@ -98,11 +98,8 @@ module.exports = [
   {
     resolve: `gatsby-plugin-netlify-cms`,
     options: {
-      /**
-       * One convention is to place your Netlify CMS customization code in a
-       * `src/cms` directory.
-       */
-      modulePath: `${__dirname}/src/cms/cms.js`,
+      modulePath: `${__dirname}/../../src/cms/cms.js`,
+      enableIdentityWidget: false,
     },
   },
   {
--- src/components/BlogPostPreview.js
+++ src/components/BlogPostPreview.js
@@ -1,7 +1,57 @@
-import Body from "~/src/components/blog/post/body"
+import { useState } from "react"
+import unified from "unified"
+import remarkParse from "remark-parse"
+import remarkHtml from "remark-html"
+import { useEffect } from "react"
 
-const BlogPostPreview = ({ widgetFor }) => {
-  return <Body html={widgetFor("body")} />
+import TitleWrapper from "~/src/components/blog/post/wrapper"
+import Wrapper from "~/src/components/blog/post/wrapper"
+import BodyWrapper from "~/src/components/blog/post/body_wrapper"
+import Body from "~/src/components/blog/post/body"
+import "~/src/common/base.scss"
+import Title from "~/src/components/blog/title"
+
+import * as styles from "~/src/templates/blog/post.module.scss"
+import * as titleStyles from "~/src/components/blog/post/header.module.css"
+
+const BlogPostPreview = (props) => {
+  const md = props.entry.getIn(["data", "body"])
+  const [html, setHTML] = useState("")
+
+  useEffect(() => {
+    console.log("jaskdlfjalsdf")
+    ;(async () => {
+      const { contents } = await unified()
+        .use(remarkParse)
+        .use(remarkHtml)
+        .process(md)
+
+      setHTML(contents)
+    })()
+  }, [md])
+
+  return (
+    <div className={styles.root} style={{ paddingTop: "10px" }}>
+      <link rel="stylesheet" href="https://use.typekit.net/dpm7mos.css" />
+
+      <article className={styles.article}>
+        <section>
+          <Wrapper className={styles.outerWrapper}>
+            <header className={styles.header}>
+              <TitleWrapper padded>
+                <div className={titleStyles.title}>
+                  <Title>{props.entry.getIn(["data", "title"])}</Title>
+                </div>
+              </TitleWrapper>
+            </header>
+            <BodyWrapper className={styles.innerWrapper}>
+              <Body html={html} />
+            </BodyWrapper>
+          </Wrapper>
+        </section>
+      </article>
+    </div>
+  )
 }
 
 export default BlogPostPreview
