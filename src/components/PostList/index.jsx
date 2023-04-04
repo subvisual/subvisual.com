4f9910994f727ac881234363bb2d0bcfb1898293
Gabriel Poca <mail@gabrielpoca.com>
2023-04-04 12:58:24 +0100

    Show categories on the index page

--- src/components/PostList/index.jsx
+++ src/components/PostList/index.jsx
@@ -2,14 +2,20 @@ import React from "react"
 import PropTypes from "prop-types"
 import dateFormat from "dateformat"
 import { Link } from "gatsby"
+// import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import * as styles from "./index.module.scss"
+import Categories from "../Categories"
 
-function Author({ author: { key, name } }) {
-  return <Link to={`/blog/author/${key}`}>By {name}</Link>
+function Author({ author: { key, name }, ...props }) {
+  return (
+    <Link {...props} to={`/blog/author/${key}`}>
+      {name}
+    </Link>
+  )
 }
 
-function Entry({ author, date, intro, path, title }) {
+function Entry({ author, date, intro, path, title, categories }) {
   const formattedDate = dateFormat(date, "mmmm d, yyyy")
 
   return (
@@ -23,9 +29,28 @@ function Entry({ author, date, intro, path, title }) {
         </Link>
       </p>
       <div className={styles.info}>
-        {author && <Author className={styles.author} author={author} />}
-        <span className={styles.date}>On {formattedDate}</span>
+        {/* {author?.photo && (
+          <GatsbyImage
+            alt=""
+            image={getImage(author.photo.vertical)}
+            style={{
+              marginRight: 16,
+              height: 40,
+              width: 40,
+              borderRadius: "50%",
+            }}
+          />
+        )} */}
+        <div>
+          {author && <Author className={styles.author} author={author} />}
+          <span className={styles.date}>On {formattedDate}</span>
+        </div>
       </div>
+      {categories?.length && (
+        <div className={styles.categories}>
+          <Categories categories={categories || []} />
+        </div>
+      )}
     </div>
   )
 }
@@ -33,9 +58,9 @@ function Entry({ author, date, intro, path, title }) {
 function PostsList({ posts }) {
   return (
     <ol className={styles.posts}>
-      {posts.map(({ author, date, intro, path, title }) => (
-        <li key={path} className={styles.item}>
-          <Entry {...{ author, date, intro, path, title }} />
+      {posts.map((post) => (
+        <li key={post.path} className={styles.item}>
+          <Entry {...post} />
         </li>
       ))}
     </ol>
