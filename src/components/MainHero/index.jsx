df9f6b1bee8873b538959536348d0bd487268705
Gabriel Poca <mail@gabrielpoca.com>
2023-07-10 16:17:03 +0100

    Hotfix: use home header everywhere and fix padding

--- src/components/MainHero/index.jsx
+++ src/components/MainHero/index.jsx
@@ -1,33 +1,46 @@
 import React from "react"
-import { useLocation } from "@reach/router"
+// import { useLocation } from "@reach/router"
 import { StaticImage } from "gatsby-plugin-image"
 
 import * as styles from "./index.module.scss"
 
 function MainHero({ children }) {
-  const location = useLocation()
+  // const location = useLocation()
+  // return (
+  //   <div className={styles.root}>
+  //     {location.pathname === "/blog/" ? (
+  //       <StaticImage
+  //         src="../../images/header-home.jpg"
+  //         className={styles.imageWrapper}
+  //         imgClassName={styles.image}
+  //         layout="fullWidth"
+  //         height={816}
+  //         quality={90}
+  //       />
+  //     ) : (
+  //       <StaticImage
+  //         src="../../images/header-post.jpg"
+  //         className={styles.imageWrapper}
+  //         imgClassName={styles.image}
+  //         layout="fullWidth"
+  //         height={816}
+  //         quality={90}
+  //       />
+  //     )}
+  //     <div className={styles.content}>{children}</div>
+  //   </div>
+  // )
 
   return (
     <div className={styles.root}>
-      {location.pathname === "/blog/" ? (
-        <StaticImage
-          src="../../images/header-home.jpg"
-          className={styles.imageWrapper}
-          imgClassName={styles.image}
-          layout="fullWidth"
-          height={816}
-          quality={90}
-        />
-      ) : (
-        <StaticImage
-          src="../../images/header-post.jpg"
-          className={styles.imageWrapper}
-          imgClassName={styles.image}
-          layout="fullWidth"
-          height={816}
-          quality={90}
-        />
-      )}
+      <StaticImage
+        src="../../images/header-home.jpg"
+        className={styles.imageWrapper}
+        imgClassName={styles.image}
+        layout="fullWidth"
+        height={816}
+        quality={90}
+      />
       <div className={styles.content}>{children}</div>
     </div>
   )
